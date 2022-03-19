5c9b9a6faf6909b9cb28d8942dc1be15bb48ff22
Rick Winfrey <rick.winfrey@gmail.com>
2022-03-18 19:08:22 -0700

    Check in WIP example.rb
    Co-Authored-By: Rebecca Valentine <171941+BekaValentine@users.noreply.github.com>

--- example.rb
+++ example.rb
@@ -8,3 +8,21 @@ b = (a = 2)
   a
 # ^ defined: 1, 5
 
+if a
+#  ^ defined: 1, 5
+  b
+# ^ defined: 5
+  c = 10
+else
+  a
+# ^ defined: 1, 5
+  c = 100
+end
+
+  c
+# ^ defined: 15, 19
+#
+d = c
+  d
+# ^ defined: 25, 15, 19
+
